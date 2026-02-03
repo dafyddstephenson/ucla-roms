4784214e6ac50fe6a18201a6af432fcf02d32faf
Dafydd Stephenson <dafydd_stephenson@hotmail.com>
2026-02-03 00:46:01 +0000

    update failure scripts for github

--- ci/print_output_logs.sh
+++ ci/print_output_logs.sh
@@ -2,46 +2,45 @@
 
 #
 
-# Get the "Examples" array from Examples/code_check/do_test_all.sh
+# Get the "tests" array from tests/scripts/do_test_all.sh
 {
   IFS= read -r shebang_line
   while IFS= read -r line || [[ -n "$line" ]]; do
     [[ "$line" == *"USER INPUT END"* ]] && break
     eval "$line"
   done
-} < $ROMS_ROOT/Examples/code_check/do_test_all.sh
+} < $ROMS_ROOT/tests/scripts/do_test_all.sh
 
 if [ ${#Examples[@]} -eq 0 ]; then
-    echo "The Examples array is empty."
-    echo "This script expects a bash array `Examples` to be defined in"
-    echo "Examples/code_check/do_test_all.sh"
+    echo "The tests array is empty."
+    echo "This script expects a bash array `tests` to be defined in"
+    echo "tests/code_check/do_test_all.sh"
     echo "above the line containing `USER INPUT END`."
     echo "Please verify the contents of this file."
     exit 1
 fi
 
-# Loop over the "Examples" array
-for example in "${Examples[@]}"; do
+# Loop over the "tests" array
+for example in "${tests[@]}"; do
     echo "###############################################################################################"
     echo "$example"
     echo "###############################################################################################"
-    if [ -e "${ROMS_ROOT}/Examples/${example}/code_check/test_old.log" ];then
-	cat ${ROMS_ROOT}/Examples/${example}/code_check/test_old.log
+    if [ -e "${ROMS_ROOT}/tests/${example}/test_old.log" ];then
+	cat ${ROMS_ROOT}/tests/${example}/test_old.log
     fi
     if $example == "bgc_real";then
        echo "------------------------------"
        echo "MARBL"
        echo "------------------------------"
-       if [ -e "${ROMS_ROOT}/Examples/${example}/code_check/test_old_MARBL.log" ] ;then
-	   cat ${ROMS_ROOT}/Examples/${example}/code_check/test_old_MARBL.log
+       if [ -e "${ROMS_ROOT}/tests/${example}/test_old_MARBL.log" ] ;then
+	   cat ${ROMS_ROOT}/tests/${example}/test_old_MARBL.log
        fi
        echo "------------------------------"
        echo "BEC"
        echo "------------------------------"
-       if [ -e "${ROMS_ROOT}/Examples/${example}/code_check/test_old_BEC.log" ];then
-	   cat ${ROMS_ROOT}/Examples/${example}/code_check/test_old_BEC.log
+       if [ -e "${ROMS_ROOT}/tests/${example}/test_old_BEC.log" ];then
+	   cat ${ROMS_ROOT}/tests/${example}/test_old_BEC.log
        fi
     fi
 
-    #ls ${ROMS_ROOT}/Examples/${example}/code_check/test_old.log
 done
